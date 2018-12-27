5889cd52f7a6cc7a985cd36e5dbf06b766827f96
lidan <>
2018-12-27 18:12:14 +0800

    n00007 modify

--- n00007/n00007.go
+++ n00007/n00007.go
@@ -8,11 +8,11 @@ func reverse (x int) (r int) {
 	for x != 0{
 		r = r * 10 +  x % 10
 		x = x / 10
+		if r < math.MinInt32 || r > math.MaxInt32{
+			return 0
+		}
 	}
 
-	if r < math.MinInt32 || r > math.MaxInt32{
-		return 0
-	}
 	return r
 }
 
