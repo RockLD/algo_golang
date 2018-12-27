e509553c5d20d0fdcab2a344522b1350372d6e24
lidan <>
2018-12-27 18:09:42 +0800

    n00007

---
+++ n00007/n00007.go
@@ -0,0 +1,18 @@
+package n00007
+
+import "math"
+
+func reverse (x int) (r int) {
+
+
+	for x != 0{
+		r = r * 10 +  x % 10
+		x = x / 10
+	}
+
+	if r < math.MinInt32 || r > math.MaxInt32{
+		return 0
+	}
+	return r
+}
+
