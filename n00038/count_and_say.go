9c790f17e103e54c953b4473bf56faad9606a9aa
RockLD <lid18149@gmail.com>
2019-08-20 11:10:58 +0800

    38

--- n00038/count_and_say.go
+++ n00038/count_and_say.go
@@ -1,5 +1,43 @@
 package n00038
 
+import "strconv"
+
 func countAndSay(n int) string {
+	if n == 1 {
+		return "1"
+	}
+
+	ans := "11"
+
+	for n > 2 {
+		n--
+		tmp := ""
+		now := '+'
+		count := 0
+		for _,v := range  ans {
+			if now == '+' {
+				now = v
+				count++
+				continue
+			}
+
+			if now == v {
+				count++
+				continue
+			}else {
+				tmp += strconv.Itoa(count)
+				tmp += string(now)
+				count = 1
+				now = v
+				continue
+			}
+		}
+		tmp += strconv.Itoa(count)
+		tmp += string(now)
+		ans = tmp
+	}
+
+
+	return ans
 
 }
--- n00038/count_and_say_test.go
+++ n00038/count_and_say_test.go
@@ -3,6 +3,10 @@ package n00038
 import "testing"
 
 func TestCountAndSay(t *testing.T) {
-
+	n := 1
+	res := countAndSay(n)
+	if res != "1" {
+		t.Error("err")
+	}
 }
 
