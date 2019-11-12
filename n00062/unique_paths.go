1d16047e137753b708ad56c0eeae813b117829d8
RockLD <lid18149@gmail.com>
2019-11-12 21:12:14 +0800

    n00062

---
+++ n00062/unique_paths.go
@@ -0,0 +1,29 @@
+package n00062
+
+func uniquePaths(m int, n int) int {
+	if m <= 0 || n <= 0 {
+		return 0
+	}
+	// 初始化二维切片
+	dp := make([][]int,m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int,n+1)
+	}
+	// 初始化数据
+	for i := 0; i < m; i++ {
+		dp[i][0] = 1
+	}
+
+	for j := 0; j < n; j++ {
+		dp[0][j] = 1
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+
+	return dp[m-1][n-1]
+
+}
---
+++ n00062/unique_paths_test.go
@@ -0,0 +1,14 @@
+package n00062
+
+import (
+	"testing"
+)
+
+func TestUniquePaths(t *testing.T) {
+	m := 7
+	n := 3
+
+	res := uniquePaths(m, n)
+
+	t.Fatal(res)
+}
